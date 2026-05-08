a0a8113c96acee5cf08c3affeecf878b46e22d7e
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-08 14:57:14 +0900

    refactor: 질문 목록 출력을 하드코딩 HTML에서 타임리프 템플릿으로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,18 +19,27 @@ repositories {
 }
 
 dependencies {
+    // --- Spring Boot Starters (핵심 프레임워크) ---
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+
+    // --- Database (DB 관련) ---
     runtimeOnly("com.h2database:h2")
     implementation("org.springframework.boot:spring-boot-h2console")
+
+    // --- Tools (도구 및 유틸리티) ---
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // --- Testing (테스트 관련) ---
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
     testCompileOnly("org.projectlombok:lombok")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testAnnotationProcessor("org.projectlombok:lombok")
-    implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
-    testImplementation ("org.springframework.boot:spring-boot-starter-data-jpa-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
