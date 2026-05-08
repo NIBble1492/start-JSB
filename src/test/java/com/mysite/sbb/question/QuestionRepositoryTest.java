e973205d6a6caf62ca7641eedbc4a6ed20f40821
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-08 14:09:58 +0900

    refactor: 도메인(question, answer)별로 패키지 구조 분리

--- src/test/java/com/mysite/sbb/question/QuestionRepositoryTest.java
+++ src/test/java/com/mysite/sbb/question/QuestionRepositoryTest.java
@@ -1,23 +1,23 @@
-package com.mysite.sbb;
+package com.mysite.sbb.question;
 
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.answer.AnswerRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 @SpringBootTest
 @Transactional
-class PostRepositoryTest  {
+class QuestionRepositoryTest {
     @Autowired
     private QuestionRepository questionRepository;
 
