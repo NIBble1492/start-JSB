9c6b6f522f0bd86c12e090bfba3bac5a9ecaa8db
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-08 14:30:50 +0900

    feat: 질문 도메인 컨트롤러 추가

--- src/main/java/com/mysite/sbb/TestInitData.java
+++ src/main/java/com/mysite/sbb/TestInitData.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
-@Profile("test")
 @Configuration
 @RequiredArgsConstructor
 public class TestInitData {
