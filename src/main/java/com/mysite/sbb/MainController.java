8be63c3e3e27914deed1e7383106acc0a88c2884
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-11 10:36:39 +0900

    feat: 루트 URL 접속 시 질문 목록으로 리다이렉트 설정

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -7,14 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
     @GetMapping("/")
-    @ResponseBody
     public String index() {
-        return "SBB";
-    }
-
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "안녕하세요!";
+        return "redirect:/question/list";
     }
 }
