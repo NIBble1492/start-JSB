bcbeb9a8f98a7805677b9d9ae27b82c5bc4c77e8
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-11 11:47:18 +0900

    feat: 답변 기능 추가

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb.question;
 
+import com.mysite.sbb.answer.Answer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -18,7 +21,7 @@ public class QuestionController {
 
     @GetMapping("/list")
     public String list(Model model) {
-        List<Question> questionList = this.questionService.getList();
+        List<Question> questionList = questionService.getList();
         model.addAttribute("questionList", questionList);
 
         return "question_list";
@@ -27,6 +30,10 @@ public class QuestionController {
     @GetMapping("/detail/{id}")
     public String detail(Model model, @PathVariable int id) {
         Question question = questionService.getQuestion(id);
+
+        // 원본 리스트를 복사해서 역순 정렬 (원본 훼손 방지)
+        List<Answer> reversedAnswers = new ArrayList<>(question.getAnswers());
+        Collections.reverse(reversedAnswers);
 
         model.addAttribute("question", question);
         return "question_detail";
